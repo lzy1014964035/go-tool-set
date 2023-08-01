b589f0e35f13207001b5a608a01ebf7659863e7a
陆泽宇 <1014964035@qq.com>
2023-08-01 17:17:13 +0800

    【更新】

--- service/request/http/httpService.go
+++ service/request/http/httpService.go
@@ -1,14 +1,26 @@
 package http
 
 import (
+	"encoding/json"
 	"net/http"
 	"github.com/lzy1014964035/go-tool-set/service"
 )
 
+type ResponseWriter = http.ResponseWriter
+type Request        = http.Request
+type ReuqestData    = service.ToMap
+
 // 添加路由
-func AddUrl(url string, callableFunction func(http.ResponseWriter, *http.Request)){
-	http.HandleFunc(url, func(ResponseWriter http.ResponseWriter, Request *http.Request){
-		callableFunction(ResponseWriter, Request);
+func AddUrl(url string, callableFunction func(http.ResponseWriter, *http.Request, ReuqestData)){
+	http.HandleFunc(url, func(w http.ResponseWriter, r *http.Request){
+		var data ReuqestData
+		err := json.NewDecoder(r.Body).Decode(&data)
+		if err != nil {
+			http.Error(w, "Invalid JSON payload", http.StatusBadRequest)
+			return
+		}
+		// 获取 JSON 中的字段值
+		callableFunction(w, r, data);
 	})
 }
 
@@ -20,4 +32,27 @@ func MakeHttpService(listenPort string){
 // 创建HTTPS服务器
 func MakeHttpsService(listenPort string, sslPemPath string, sslKeyPath string){
 	service.Fail(http.ListenAndServeTLS(":"+listenPort, sslPemPath, sslKeyPath, nil))
+}
+
+// 返回内容
+func ReturnSuccess(w http.ResponseWriter, message string, data service.ToMap){
+	ReturnData(w, "success", "200", message, data);
+}
+
+// 返回失败
+func ReturnFail(w http.ResponseWriter, message string, data service.ToMap){
+	ReturnData(w, "fail", "405", message, data);
+}
+
+// 返回结构
+func ReturnData(w http.ResponseWriter, status string, code string, message string, data service.ToMap){
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(service.JsonEncode(service.ToMap{
+		"status": "fail",
+		"code": code,
+		"result": service.ToMap{
+			"msg": message,
+			"data": data,
+		},
+	})));
 }
